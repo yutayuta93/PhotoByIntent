f4802063172b2f15dcc26f59cfcba1f26e403c32
yuta <yutatakeuchi3721@gmail.com>
2019-02-07 11:04:17 +0900

    【実習】サムネイルを取得する  を実装

--- app/src/main/java/com/example/android/photobyintent/MainActivity.java
+++ app/src/main/java/com/example/android/photobyintent/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -18,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int ACTION_TAKE_PHOTO_SMALL = 2;
     private static final int ACTION_TAKE_VIDEO = 3;
 
-
+    private ImageView mImageView;
+    private Bitmap mImageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         Button btnIntent = (Button)findViewById(R.id.btnIntent);
         setBtnListenerOrDisable(btnIntent,createOnClickListener(ACTION_TAKE_PHOTO),MediaStore
                 .ACTION_IMAGE_CAPTURE);
+
+        mImageView = (ImageView)findViewById(R.id.imageView1);
 
     }
 
@@ -70,6 +77,49 @@ public class MainActivity extends AppCompatActivity {
         List<ResolveInfo> list = packageManager.queryIntentActivities(intent,PackageManager
                 .MATCH_DEFAULT_ONLY);
         return list.size() > 0;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case ACTION_TAKE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    handleCameraPhoto(data);
+                }
+                break;
+
+            case ACTION_TAKE_PHOTO_SMALL:
+                if(resultCode == RESULT_OK){
+
+                }
+                break;
+
+            case ACTION_TAKE_VIDEO:
+                if(resultCode == RESULT_OK){
+
+                }
+                break;
+        }
+    }
+
+
+    private void handleCameraPhoto(Intent intent){
+        Uri data = intent.getData();
+        if(data != null) {
+            Log.d("RESULT_DATA", data.toString());
+        }
+        Bundle bundle = intent.getExtras();
+        /*
+        Set<String> keys = bundle.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while(iterator.hasNext()){
+            Log.d("KEY",iterator.next());
+        }
+        */
+        mImageBitmap =(Bitmap)bundle.get("data");
+        mImageView.setImageBitmap(mImageBitmap);
+        mImageView.setVisibility(View.VISIBLE);
+
     }
 }
 
